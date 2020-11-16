76bf9fa4967f28d9e178019d8229a75a79b7e7e0
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-16 19:56:39 +0530

    Added: Win or Loose function

---
+++ gamblingProblem.js
@@ -0,0 +1,27 @@
+console.log("Welcome to Gambling simulation!");
+let dailyStake = 100;
+let everyGameBet = 1;
+class GamblingProblem(){
+    constructor(win, lose){
+        this.win = win;
+        this.lose = lose;
+    }
+    gambleWinOrLoose(){
+        return Math.floor(Math.random() * Math.floor(1));
+    }
+    checkWinOrLoose(){
+        let score = this.gambleWinOrLoose();
+
+        if (score == 1) {
+            this.win += 1;
+            dailyStake += 1;
+        }
+        if (score == 0) {
+            this.lose += 1;
+            dailyStake -= 1;
+        }
+
+    }
+
+}
+
