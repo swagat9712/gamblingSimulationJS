05539f8ac7d7749000b0c982517176360d1e8298
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-16 21:58:19 +0530

    Refactored: the main branch

--- gamblingProblem.js
+++ gamblingProblem.js
@@ -1,27 +1,4 @@
+import * as Utility from "./util.js";
 console.log("Welcome to Gambling simulation!");
-let dailyStake = 100;
-let everyGameBet = 1;
-class GamblingProblem {
-    constructor(win, lose){
-        this.win = win;
-        this.lose = lose;
-    }
-    gambleWinOrLoose(){
-        return Math.floor(Math.random() * Math.floor(1));
-    }
-    checkWinOrLoose(){
-        let score = this.gambleWinOrLoose();
-        while (dailyStake != 150 && dailyStake != 50) {
-            if (score == 1) {
-                this.win += 1;
-                dailyStake += 1;
-            }
-            if (score == 0) {
-                this.lose += 1;
-                dailyStake -= 1;
-            }
-        }
-
-    }
-
-}
+let GamblingProblem = Utility.default;
+new GamblingProblem();
