169ef3aee28e38f944ebedfd968de8157950f74f
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-17 20:18:06 +0530

    Refactored: luckiest and unluckiest number

--- util.js
+++ util.js
@@ -1,14 +1,15 @@
-export let dailyStake = 100;
-export let totalDays = 0;
-export let totalAmount = 0;
-export let bestWin = 0;
-export let bestLose = 0;
-export let luckiestDay = 0;
-export let unluckiestDay = 0;
-export default class GamblingProblem {
-    constructor(win, lose) {
-        this.win = win;
-        this.lose = lose;
+
+class GamblingProblem {
+    constructor() {
+        this.win = 0;
+        this.lose = 0;
+        this.dailyStake = 100;
+        this.totalDays = 0;
+        this.totalAmount = 0;
+        this.bestWin = 0;
+        this.bestLose = 0;
+        this.luckiestDay = 0;
+        this.unluckiestDay = 0;
     }
     //taking a random value 0 or 1// 
     gambleWinOrLoose() {
@@ -17,50 +18,53 @@ export default class GamblingProblem {
     //takeing win or loose//
     checkWinOrLoose() {
         try {
-            while (dailyStake != 150 && dailyStake != 50) {
+            while (this.dailyStake != 150 && this.dailyStake != 50) {
                 let score = this.gambleWinOrLoose();
                 if (score == 1) {
                     this.win++;
-                    dailyStake++;
+                    this.dailyStake++;
                 }
                 if (score == 0) {
                     this.lose++;
-                    dailyStake--;
+                    this.dailyStake--;
                 }
             }
-            console.log("win: " + this.win + " lose: " + this.lose + " Earned: " + dailyStake);
-            if (this.win > bestWin && dailyStake == 150) {
-                bestWin = this.win;
-                luckiestDay = totalDays;
+            console.log("win: " + this.win + " lose: " + this.lose + " Earned: " + this.dailyStake);
+            if (this.win > this.bestWin && this.dailyStake == 150) {
+                this.bestWin = this.win;
+                this.luckiestDay = this.totalDays;
             }
-            if (this.lose > bestLose && dailyStake == 50) {
-                bestLose = this.lose;
-                unluckiestDay = totalDays;
+            if (this.lose > this.bestLose && this.dailyStake == 50) {
+                this.bestLose = this.lose;
+                this.unluckiestDay = this.totalDays;
             }
             this.win = 0;
             this.lose = 0;
         } catch (e) {
-            console.log("Exception: " + e);
+            console.log("Exception1: " + e);
         }
     }
     //Calling the checkWinOrLoose method 20 times//
     resultAfterNumbreOfDays() {
         try {
-            while (totalDays < 30) {
-                totalDays++;
-                console.log("Day:" + totalDays);
+            while (this.totalDays < 30) {
+                this.totalDays++;
+                console.log("Day:" + this.totalDays);
                 this.checkWinOrLoose();
-                totalAmount = totalAmount + dailyStake - 100;
-                dailyStake = 100;
+                this.totalAmount = this.totalAmount + this.dailyStake - 100;
+                this.dailyStake = 100;
 
             }
-            totalAmount = totalAmount + 100;
-            console.log("Total Days: " + totalDays + " Total amount: " + totalAmount);
-            console.log("Luckiest Day: " + luckiestDay + " Unluckiest Day: " + unluckiestDay);
+            this.totalAmount = this.totalAmount + 100;
+            console.log("Total Days: " + this.totalDays + " Total amount: " + this.totalAmount);
+            console.log("Luckiest Day: " + this.luckiestDay + " Unluckiest Day: " + this.unluckiestDay);
         }
         catch (e) {
-            console.log("Exception: " + e);
+            console.log("Exception2: " + e);
         }
 
     }
 }
+
+module.exports = new GamblingProblem();
+
