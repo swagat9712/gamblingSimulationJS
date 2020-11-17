51e8703e0db367d90d120dd6b4216f56828f301c
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-17 20:16:59 +0530

    Refactored: main file

--- gamblingProblem.js
+++ gamblingProblem.js
@@ -1,7 +1,4 @@
-import * as Utility from "./Utility.js";
-console.log("Welcome to Sanke and Ladders!");
-let SnakeAndLadders = Utility.default;
-new SnakeandLadder();
-Utility.playerPosition1();
-Utility.playerPosition2();
-Utility.calculateWin();
+let utility = require("./util.js");
+console.log("Welcome to Gambling simulation!");
+
+utility.resultAfterNumbreOfDays();
