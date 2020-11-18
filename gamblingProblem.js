ba683b06909d04d9b08145b5efbd507bfeeb857c
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-18 12:15:17 +0530

    Added: For Continue next month

--- gamblingProblem.js
+++ gamblingProblem.js
@@ -1,3 +1,13 @@
 let utility = require("./util.js");
 console.log("Welcome to Gambling simulation!");
 utility.resultAfterNumbreOfDays();
+let choice = window.prompt("Enter if you want to continue next month(yes/no): ");
+if(choice == "yes"){
+    utility.resultAfterNumbreOfDays();
+}
+if(choice == "no"){
+    console.log("Good Gambling!");
+} 
+else{
+    console.log("Wrong Choice!");
+}
