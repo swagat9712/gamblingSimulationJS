a375c19029eb63bc43abac028a4a32465d8c966b
Swagat Bhattacharjee <66122924+swagat1297@users.noreply.github.com>
2020-11-18 13:00:34 +0530

    Refactored: to iterate till terminated

--- gamblingProblem.js
+++ gamblingProblem.js
@@ -1,13 +1,22 @@
 let utility = require("./util.js");
 console.log("Welcome to Gambling simulation!");
 utility.resultAfterNumbreOfDays();
-let choice = window.prompt("Enter if you want to continue next month(yes/no): ");
-if(choice == "yes"){
-    utility.resultAfterNumbreOfDays();
+let isTerminated = false;
+try {
+    while (isTerminated != true) {
+        let choice = window.prompt("Enter if you want to continue next month(yes/no): ");
+        if (choice == "yes") {
+            utility.resultAfterNumbreOfDays();
+        }
+        if (choice == "no") {
+            console.log("Good Gambling!");
+            isTerminated = true;
+        }
+        else {
+            console.log("Wrong Choice!");
+        }
+    }
 }
-if(choice == "no"){
-    console.log("Good Gambling!");
-} 
-else{
-    console.log("Wrong Choice!");
+catch (e) {
+    console.log("Exception:" + e);
 }
